2fafa25d8c56afd50058d67b953f510af8d7d074
Christopher Ochsenreither <replmade@gmail.com>
2023-06-04 19:59:05 -0700

    memory buffer window

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain";
 
-import { memory, createChainWithModel } from "./memory";
+import {
+  memory,
+  mementoMemory,
+  createChainWithModel,
+  createMementoWithModel,
+} from "./memory";
 
 dotenv.config();
 
@@ -12,6 +17,7 @@ const model = new OpenAI({
 });
 
 (async () => {
+  // Conversation with memory buffer
   const chain = createChainWithModel(model, memory);
   const res1 = await chain.call({
     input: "Hi, my name is Chris",
@@ -25,4 +31,42 @@ const model = new OpenAI({
     input: "What is my name?",
   });
   console.log(res3);
+  console.log(memory);
+
+  // Conversation with memory buffer of k=3
+  const memento = createMementoWithModel(model, mementoMemory);
+  const res4 = await memento.call({
+    input: "Have I told you about my condition?",
+  });
+  console.log(res4);
+  const res5 = await memento.call({
+    input: "I have no memory.",
+  });
+  console.log(res5);
+  const res6 = await memento.call({
+    input: `I have no short-term memory. I know who I am\
+    and all about myself, but since my injury I can't make\
+    any new memories.`,
+  });
+  console.log(res6);
+  const res7 = await memento.call({
+    input: "I've told you this before, haven't I?",
+  });
+  console.log(res7);
+  const res8 = await memento.call({
+    input: "What's the last thing you remember?",
+  });
+  console.log(res8);
+  const res9 = await memento.call({
+    input: "My name is Leonard",
+  });
+  console.log(res9);
+  const res10 = await memento.call({
+    input: "What is 1+1?",
+  });
+  console.log(res10);
+  const res11 = await memento.call({
+    input: "Have I told you about my condition?",
+  });
+  console.log(res11);
 })();
--- src/memory.ts
+++ src/memory.ts
@@ -1,10 +1,23 @@
 import { OpenAI } from "langchain";
-import { BufferMemory } from "langchain/memory";
+import { BufferMemory, BufferWindowMemory } from "langchain/memory";
 import { ConversationChain } from "langchain/chains";
 
 export const memory = new BufferMemory();
+export const mementoMemory = new BufferWindowMemory({ k: 3 });
 
-export const createChainWithModel = (model, memory) =>
+export const createChainWithModel = (
+  model: OpenAI,
+  memory: BufferMemory
+): ConversationChain =>
+  new ConversationChain({
+    llm: model,
+    memory,
+  });
+
+export const createMementoWithModel = (
+  model: OpenAI,
+  memory: BufferWindowMemory
+): ConversationChain =>
   new ConversationChain({
     llm: model,
     memory,
